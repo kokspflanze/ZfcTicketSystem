d1426bbe58588992069775128fcd78197ff5643c
kokspflanze <kokspflanze@gmx.de>
2014-08-09 15:03:06 +0200

    Module (ZfcTicketSystem) could not be initialized

--- src/ZfcTicketSystem/Module.php
+++ src/ZfcTicketSystem/Module.php
@@ -1,7 +1,58 @@
 <?php
 /**
- * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
- * It allows usage of this module even without composer.
- * The original Module.php is in 'src/BjyAuthorize' in order to respect PSR-0
+ * Created by PhpStorm.
+ * User: †KôKšPfLâÑzè®
+ * Date: 07.08.14
+ * Time: 18:36
  */
-require_once __DIR__ . '/src/ZfcTicketSystem/Module.php';
+
+namespace ZfcTicketSystem;
+
+class Module {
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+
+	/**
+	 * Expected to return \Zend\ServiceManager\Config object or array to
+	 * seed such an object.
+	 *
+	 * @return array|\Zend\ServiceManager\Config
+	 */
+	public function getServiceConfig() {
+		return array(
+			'invokables' => array(
+				'zfcticketsystem_ticketsystem_service'		=> 'ZfcTicketSystem\Service\TicketSystem',
+			),
+			'factories' => array(
+				'zfcticketsystem_ticketsystem_new_form' => function($sm){
+						/** @var $sm \Zend\ServiceManager\ServiceLocatorInterface */
+						/** @var $oRepositoryCategory \Doctrine\Common\Persistence\ObjectRepository */
+						$oRepositoryCategory = $sm->get('Doctrine\ORM\EntityManager')->getRepository('ZfcTicketSystem\Entity\Ticketcategory');
+						$form = new Form\TicketSystem($sm->get('Doctrine\ORM\EntityManager'));
+						$form->setInputFilter(new Form\TicketSystemFilter(
+							new \PServerCMS\Validator\RecordExists( $oRepositoryCategory, 'categoryId' )
+						));
+						return $form;
+					},
+				'zfcticketsystem_ticketsystem_entry_form' => function($sm){
+						/** @var $sm \Zend\ServiceManager\ServiceLocatorInterface */
+						/** @var $oRepositoryCategory \Doctrine\Common\Persistence\ObjectRepository */
+						$form = new Form\TicketEntry();
+						$form->setInputFilter(new Form\TicketEntryFilter());
+						return $form;
+					}
+			),
+		);
+	}
+}
